4810d9fc06c8623fa7ced5848f58e31b7d5ea7de
toddy <toddyyang1990@foxmail.com>
2021-11-30 13:56:07 +0800

    显示样式上做了修改

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 import pprint
 import sys
 import os
-import traceback
-import copy
 
 CGTW_ROOT_BIN = u"C:/CgTeamWork_v6.2/bin/"
 #CGTW_ROOT_BIN = __file__.replace(u"\\", u"/").split(u"ext_plugin")[0]
@@ -14,7 +12,6 @@ for _path in [
     CGTW_ROOT_BIN + u"base/com_lib",
     CGTW_ROOT_BIN + u"base/com_icon"
 ]:
-    print(_path)
     _path in sys.path or sys.path.append(_path)
 
 # pyside
@@ -35,28 +32,23 @@ class Info_Widget(QtWidgets.QDialog):
         self.main_layout = QtWidgets.QVBoxLayout(self)
         text_browser = QtWidgets.QTextBrowser()
         self.main_layout.addWidget(text_browser)
-        self.resize(400, 210)
+        self.resize(500, 300)
 
         t_tw = cgtw2.tw()
-        text_browser.append(u"id:            %s" % str(t_tw.client.get_id()))
-        text_browser.append(u"database:      %s" % str(t_tw.client.get_database()))
-        text_browser.append(u"module:        %s" % str(t_tw.client.get_module()))
-        text_browser.append(u"module_type:   %s" % str(t_tw.client.get_module_type()))
-        text_browser.append(u"-" * 50)
-        text_browser.append(u"link_id:       %s" % str(t_tw.client.get_link_id()))
-        text_browser.append(u"link_module:   %s" % str(t_tw.client.get_link_module()))
-        text_browser.append(u"filebox_id:    %s" % str(t_tw.client.get_filebox_id()))
-        text_browser.append(u"-" * 50)
-        text_browser.append(u"event_action:  %s" % str(t_tw.client.get_event_action()))
-        text_browser.append(u"event_fields:  %s" % str(t_tw.client.get_event_fields()))
-        text_browser.append(u"event_fields:  %s" % str(t_tw.client.get_event_fields()))
-        text_browser.append(u"-" * 50)
-        text_browser.append(u" -------------")
-        text_browser.append(u"| get_sys_key |")
-        text_browser.append(u" -------------")
+        text_browser.append(u"client.get_id:            %s" % str(t_tw.client.get_id()))
+        text_browser.append(u"client.get_database:      %s" % str(t_tw.client.get_database()))
+        text_browser.append(u"client.get_module:        %s" % str(t_tw.client.get_module()))
+        text_browser.append(u"client.get_module_type:   %s" % str(t_tw.client.get_module_type()))
+        text_browser.append(u"client.get_link_id:       %s" % str(t_tw.client.get_link_id()))
+        text_browser.append(u"client.get_link_module:   %s" % str(t_tw.client.get_link_module()))
+        text_browser.append(u"client.get_filebox_id:    %s" % str(t_tw.client.get_filebox_id()))
+        text_browser.append(u"client.get_event_action:  %s" % str(t_tw.client.get_event_action()))
+        text_browser.append(u"client.get_event_fields:  %s" % str(t_tw.client.get_event_fields()))
+        text_browser.append(u"client.get_event_fields:  %s" % str(t_tw.client.get_event_fields()))
+        text_browser.append(u"-" * 75)
         argv = _client._client__get_argv_data()
         for k, v in argv.items():
-            text_browser.append(u"%s:  %s" % (str(k), str(v)))
+            text_browser.append(u"client.get_sys_key(\"%s\"):   %s" % (str(k), str(v)))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
